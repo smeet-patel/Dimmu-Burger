d083e424b1b0158335335bf3188aa921dc128375
Smeet <smeetprpatel@gmail.com>
2018-11-11 17:24:50 +1300

    UI form update

--- javascript/script.js
+++ javascript/script.js
@@ -9,6 +9,17 @@ function myFunction() {
     }
 }
 //
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
+
+
 $(document).ready(function($) {
 
 	$('.card__share > a').on('click', function(e){ 
@@ -46,12 +57,3 @@ var progressBar = {
   $("#Reset").on('click', function(){
     progressBar.Reset();
   })
-
-
-  
-var slider = document.getElementById("comment");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value;
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
